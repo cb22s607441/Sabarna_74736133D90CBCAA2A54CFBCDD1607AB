83f62f2092740c8a09cf53b37d9700e80cde8aa2
cb22s607441 <144345124+cb22s607441@users.noreply.github.com>
2023-09-27 19:18:29 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,39 @@
-#1.2 Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
 
-year=int(input("Input the Year: "))               #input year 
-if(year%4==0):                                    #check whether year is divisible by 4 or not
-    if(year%400==0):                              #if year is divisible by 400 
-        print("Entered Year is a Leap Year.")     
-    elif(year%100==0 and year%400!=0):
-        print("It is not a Leap Year.") #if divisible by 100 but not 400
-    else:
-        print("Entered Year is a Leap Year.")
-else:
-    print("Entered Year is not a Leap Year.")
+class Student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-#1.1 Implement a recursive function to calculate the factorial of a given number.
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-def factorial(n):
-    if (n==1 or n==0):
-        return 1 
-    else:
-        return n * factorial(n - 1)
- 
-num = int(input("Enter number: "))
-x=factorial(num)
-print("Factorial of",num,"is",x)
+
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
