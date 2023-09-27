56ba83385f63f4b9d2c26a63f020d6b17eb7e84d
cb22s607441 <144345124+cb22s607441@users.noreply.github.com>
2023-09-27 19:36:01 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,34 +1,11 @@
-# 2.1 '''Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.'''
+1.1  As Implement a recursive function to calculate the factorial of a given number.
 
-class BankAccount:
-
-  def __init__(self, account_number, account_holder_name, initial_balance):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+def factorial(n):
+    if (n==1 or n==0):
+        return 1 
     else:
-      print("Invalid deposit amount.")
-
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      print("Withdrew ₹{}. New balance:₹{}".format(amount, self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient  balance.")
-
-  def display_balance(self):
-    print("Account balance for {}(Account #{}): ₹{}".format( self.__account_holder_name, self.__account_number, self.__account_balance))
-
-
-account = BankAccount(account_number="123456789",account_holder_name="Lara", initial_balance=5000.0)
-
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+        return n * factorial(n - 1)
+ 
+num = int(input("Enter number: "))
+x=factorial(num)
+print("Factorial of",num,"is",x)
